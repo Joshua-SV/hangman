d33ebadee132efc6ce1b64323b0011805250d71d
Joshua-SV <J.JSerna@hotmail.com>
2023-01-01 04:07:25 -0600

    added comments

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 # display logo
 print(stageArt.logo)
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+#Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosenWord = random.choice(words.word_list)
 
 # replace string with blanks then display
@@ -14,21 +14,22 @@ display = []
 length = len(chosenWord)
 endOfgame = False
 lives = len(stageArt.stages)
-clear = lambda: os.system('clear')
+
 
 for i in range(length):
     display.append("_")
 
 print(f"{' '.join(display)}")
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+#Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 while not endOfgame:
     guess = input("Enter a letter to guess: ").lower()
 
-
+    #clear console after user guess
+    clear = lambda: os.system('clear')
     clear()
 
-    # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+    #Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
     for x in range(length):
         if guess == chosenWord[x]:
